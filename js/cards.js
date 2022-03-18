2853b13ea8efb38cd5353d7990eeae185269815f
mjwhite10 <manublanco10037@gmail.com>
2022-03-18 12:08:54 +0100

    Añadiendo panel de inicio y lógica JS

--- js/cards.js
+++ js/cards.js
@@ -1,17 +1,20 @@
 "use strict";
 
-const emojiCards = ["👩‍💻", "👻", "🔑", "🤌🏼", "🎵", "🍕", "🦜"];
+const emojiCards = ["👩‍💻", "👻", "🙈", "🔑", "🤌🏼", "🎵", "🍕", "🦜"];
+
+function duplicate(array) {
+  const copyArray = [];
+  for (let i = 0; i < array.length; i++) {
+    copyArray.push(array[i]);
+    copyArray.push(array[i]);
+  }
+  return copyArray;
+}
 
 function getRandomEmojiCards() {
-  const newArray = [];
-  //Generamos un newArray con las parejas de emojis
-  for (let i = 0; i < emojiCards.length; i++) {
-    newArray.push(emojiCards[i]);
-    newArray.push(emojiCards[i]);
-  }
   //Por último lo ordenamos el newArray
   //de forma aleatoria y lo devolvemos
-  return newArray.sort(() => 0.5 - Math.random);
+  return duplicate(emojiCards).sort(() => 0.5 - Math.random());
 }
 
 export { getRandomEmojiCards };
