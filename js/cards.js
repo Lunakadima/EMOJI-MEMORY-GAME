710bc6d56f34b437c6e6efd4216b45684906d8d4
Luna Cadima <lunakadima@yahoo.es>
2022-03-21 15:36:59 +0100

    Modificando emoticonos 2

--- js/cards.js
+++ js/cards.js
@@ -13,7 +13,7 @@ import { gameFinished } from "./main.js";
 
 let score = 0;
 let tries = 0;
-const emojiCards = ["🎮", "🛸", "👾", "🛹", "🛼", "🤖", "📟", "🕹"];
+const emojiCards = ["🎮", "🛸", "📼", "🛹", "🛼", "🤖", "📟", "🕹"];
 const flippedCardIds = [];
 const board = document.querySelector(".board");
 const cards = board.querySelectorAll(".card");
